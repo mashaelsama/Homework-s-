07fb8bd57c659e1326b0c0f405fe53a10aba1ed0
mashaelsama <114874758+mashaelsama@users.noreply.github.com>
2022-10-04 12:38:58 +0300

    Add files via upload

---
+++ kotlin.kt
@@ -0,0 +1,34 @@
+fun main() {
+    var isTrue1: Boolean = false
+    var isTrue2: Boolean = false
+    var isTrue3 = true
+    var x: Int = 10
+    var y: Int = 3
+    var z = x % y
+    var w = 5
+    var m = 2
+    var s = 6
+    var result1 = w - y * m
+    var comp = w >= w
+    var result2 = (s + s) / 3
+
+
+    println(isTrue1 && isTrue2)
+    println(z)
+    println(comp)
+    println(isTrue2 || isTrue3)
+    println(result1)
+    println(y != m)
+    println(result2)
+
+    /* results:
+    - false
+    - 1
+    - true
+    - true
+    - -1
+    - true
+    - 4
+     */
+
+}
